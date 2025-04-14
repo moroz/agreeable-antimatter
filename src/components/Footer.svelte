37114a3545339c41d868a1a123d9c7b3eaca9849
Karol Moroz <karol@moroz.dev>
2025-04-14 19:55:12 +0800

    Header/footer layout

---
+++ src/components/Footer.svelte
@@ -0,0 +1,5 @@
+<footer
+  class="flex h-[var(--footer-height)] flex-col items-center justify-center bg-green-800 text-center"
+>
+  &copy; 2025 by Karol Moroz.
+</footer>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,1 +1,10 @@
-<h3>Test</h3>
+<header class="flex h-[var(--header-height)] items-center bg-[salmon]">
+  <a href="/">
+    <h1 class="text-2xl">moroz.dev</h1>
+  </a>
+  <nav class="ml-auto">
+    <ul>
+      <li>Test</li>
+    </ul>
+  </nav>
+</header>
