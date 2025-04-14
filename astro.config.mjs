becf9b3fcd9655b4412d59f2aa2febb7dc24c66e
Karol Moroz <karol@moroz.dev>
2025-04-14 17:20:04 +0800

    Set up svelte, prettier, tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx()]
+  integrations: [mdx(), svelte()]
 });
